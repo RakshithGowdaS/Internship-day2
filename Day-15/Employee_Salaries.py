0ae551d22b579c7c328407d8bc8a1315e51f1dc0
RakshithGowdaS <109843283+RakshithGowdaS@users.noreply.github.com>
2026-02-16 22:47:07 +0530

    Add files via upload

---
+++ Day-15/Employee_Salaries.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self, name, base_salary):
+        self.name=name
+        self.base_salary=base_salary
+
+class Developer(Employee):
+    def __init__(self, name, base_salary, bonus):
+        super().__init__(name,base_salary)
+        self.bonus=bonus
+
+    def calculate_salary(self):
+        self.base_salary+=self.bonus
+        return self.base_salary
+
+d1 = Developer("A", 40000, 5000)
+print( d1.calculate_salary())
